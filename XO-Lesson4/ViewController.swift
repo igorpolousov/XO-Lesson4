0497c805d215cbffac4dc5a2c64a78d46b7f9fd4
Igor Polousov <igorpolousov@gmail.com>
2022-03-30 10:20:42 +0300

    Initial Commit

---
+++ XO-Lesson4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XO-Lesson4
+//
+//  Created by Igor Polousov on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
